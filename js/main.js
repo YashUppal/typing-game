3610f4f14eebad986dba818f28917aeffdb1e0b1
Yash Uppal <yashayash123@gmail.com>
2018-08-24 00:57:02 +0530

    final

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 window.addEventListener("load", init);
 
 // Globals
-let time = 5;
+
+// Availabe Levels
+const levels = {
+  easy: 5,
+  medium: 3,
+  hard: 2
+};
+
+// To change level
+const currentLevel = levels.medium;
+
+let time = currentLevel;
 let score = 0;
 let isPlaying;
 
@@ -43,8 +54,45 @@ const words = [
 
 // Initialize Game
 function init() {
+  // Show number of seconds in UI
+  seconds.innerHTML = currentLevel;
   // Load word from array
   showWord(words);
+  // Start matching on word input
+  wordInput.addEventListener("input", startMatch);
+  // Call countdown every second
+  setInterval(countdown, 1000);
+  // Check game status
+  setInterval(checkStatus, 50);
+}
+
+// Start match
+function startMatch() {
+  if (matchWords()) {
+    isPlaying = true;
+    time = currentLevel + 1;
+    showWord(words);
+    wordInput.value = "";
+    score++;
+  }
+
+  // If score is -1, display 0
+  if (score === -1) {
+    scoreDisplay.innerHTML = 0;
+  } else {
+    scoreDisplay.innerHTML = score;
+  }
+}
+
+// Match currentWord to wordInput
+function matchWords() {
+  if (wordInput.value === currentWord.innerHTML) {
+    message.innerHTML = "Correct!";
+    return true;
+  } else {
+    message.innerHTML = "";
+    return false;
+  }
 }
 
 // Pick and Show random word
@@ -53,4 +101,26 @@ function showWord(words) {
   const randIndex = Math.floor(Math.random() * words.length);
   // Output random word
   currentWord.innerHTML = words[randIndex];
+}
+
+// Countdown timer
+function countdown() {
+  //Make sure time is not run out
+  if (time > 0) {
+    // Decrement
+    time--;
+  } else if (time === 0) {
+    // Game is over
+    isPlaying = false;
+  }
+  // Show time
+  timeDisplay.innerHTML = time;
+}
+
+// Check game status
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = "GAME OVER.";
+    score = -1;
+  }
 }
